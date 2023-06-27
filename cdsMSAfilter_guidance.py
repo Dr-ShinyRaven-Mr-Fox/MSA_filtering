1e5a01a83764c1b3ab3d63049d66628f94edb5f8
Dr-ShinyRaven-Mr-Fox <106523578+Dr-ShinyRaven-Mr-Fox@users.noreply.github.com>
2023-06-27 23:04:00 +0200

    Add files via upload

--- cdsMSAfilter_guidance.py
+++ cdsMSAfilter_guidance.py
@@ -80,7 +80,7 @@ def cdsMSAfilter_guidance(ProjectMode,GuidanceFolder,FilteringScore,MSA,Filtered
         raise SystemExit(1)
     AlignIO.write(gfltaln,FilteredMSA,'fasta')  
 
-if __name__ == "cdsMSAfilter_guidance":    
+if __name__ == "__main__":    
     import argparse 
     parser = argparse.ArgumentParser(prog="cdsMSAfilter_guidance",description="This program allows to filter a nucleotide alignment obtained by transposing protein MSAs and performed using GUIDANCE. There are two options: filter nucleotide MSAs using the GUIDANCE results and remove gaps in MSAs. WARNING: this program needs python3 and biopython to be installed (pip install biopython)",epilog="This program is freely available, please cite Fouks et al. 2023 iScience")
     parser.add_argument("-PM", "--ProjectMode", choices =['filter+gap', 'filter', 'gap'], required=True, help="Three options are available: filter cds MSAs using guidance scores and remove gaps (filter+gap), filter cds MSAs using guidance scores only (filter), and only remove gaps (gap)") # mode: filter+gap, gap, filter
@@ -88,6 +88,6 @@ if __name__ == "cdsMSAfilter_guidance":
     parser.add_argument("-FS", "--FilteringScore", required=False, default = 0.93, type = float, help = "Please indicate the threshold you want from the GUIDANCE scores. Default is 0.93 as in GUIDANCE default setup")
     parser.add_argument("-M", "--MSA", required=True, help = "Please indicate the full path and name of the nucleotide (cds) MSA obtained (e.g. Pal2nal) by transposing amino acid MSA into nucleotide MSA")
     parser.add_argument("-FM", "--FilteredMSA", required=True, help = "Please indicate the full path and name of the output MSA")
-    args = parser.parse_args()
+    args = parser.parse_args(args)
     cdsMSAfilter_guidance(args.ProjectMode,args.GuidanceFolder,args.FilteringScore,args.MSA,args.FilteredMSA)
     
