b2c35b4d352be9ed2c5a7e757fc077ab5230396a
Dr-ShinyRaven-Mr-Fox <106523578+Dr-ShinyRaven-Mr-Fox@users.noreply.github.com>
2023-06-27 22:11:13 +0200

    Add files via upload

--- cdsMSAfilter_guidance.py
+++ cdsMSAfilter_guidance.py
@@ -11,55 +11,55 @@ from Bio import AlignIO
 import argparse 
 
 
-def cdsMSAfilter_guidance(ProjectMode,GuidanceFolder,FilteringScore,MSA,FilteredMSA):
-    def rmgap(aln):
-        lenaln = aln.get_alignment_length()
-        newaln = aln[:,:1]
-        i = 0
-        while i < lenaln:
-            if '-' in aln[:,i]:
-                 i += 3
-            else:
-                newaln = newaln + aln[:,i:i+3]
-                i += 3
-        gdaln = newaln[:,1:]
-        return gdaln
+def rmgap(aln):
+    lenaln = aln.get_alignment_length()
+    newaln = aln[:,:1]
+    i = 0
+    while i < lenaln:
+        if '-' in aln[:,i]:
+            i += 3
+        else:
+            newaln = newaln + aln[:,i:i+3]
+            i += 3
+    gdaln = newaln[:,1:]
+    return gdaln
 
-    def filtaln(caln,path):
-        rmvposS = []
-        rmvposE = []
-        for file in os.listdir(path):
-            if file.endswith('csv'):
-                with open('%s/%s' % (path,file)) as files:
-                    for line in files:
-                        if line.startswith('#'):
-                            continue
-                        col = line.split(',')[0]
-                        scor = float(line.strip().split(',')[-1])
-                        if scor < FilteringScore:
-                            remcol = int(col)
-                            strtpos = (remcol*3)-3
-                            endpos = (remcol*3)
-                            rmvposS.append(strtpos)
-                            rmvposE.append(endpos)
-                alnF = AlignIO.read('%s' % caln,'fasta')
-                ftaln = alnF[:,0:1]
-                beg = 0
-                i = 0
-                for pos in rmvposS:
-                    if rmvposS[i] == 0:
-                        beg = rmvposE[i]
-                        i += 1
+def filtaln(caln,path):
+    rmvposS = []
+    rmvposE = []
+    for file in os.listdir(path):
+        if file.endswith('csv'):
+            with open('%s/%s' % (path,file)) as files:
+                for line in files:
+                    if line.startswith('#'):
                         continue
-                    else:
-                        ftaln = ftaln + alnF[:,beg:rmvposS[i]]
-                        beg = rmvposE[i]
-                        i += 1
-                        if i == len(rmvposE):
-                            ftaln = ftaln + alnF[:,beg:]
-                gftaln = ftaln[:,1:]
-        return gftaln
-        
+                    col = line.split(',')[0]
+                    scor = float(line.strip().split(',')[-1])
+                    if scor < FilteringScore:
+                        remcol = int(col)
+                        strtpos = (remcol*3)-3
+                        endpos = (remcol*3)
+                        rmvposS.append(strtpos)
+                        rmvposE.append(endpos)
+            alnF = AlignIO.read('%s' % caln,'fasta')
+            ftaln = alnF[:,0:1]
+            beg = 0
+            i = 0
+            for pos in rmvposS:
+                if rmvposS[i] == 0:
+                    beg = rmvposE[i]
+                    i += 1
+                    continue
+                else:
+                    ftaln = ftaln + alnF[:,beg:rmvposS[i]]
+                    beg = rmvposE[i]
+                    i += 1
+                    if i == len(rmvposE):
+                        ftaln = ftaln + alnF[:,beg:]
+            gftaln = ftaln[:,1:]
+    return gftaln
+
+def cdsMSAfilter_guidance(ProjectMode,GuidanceFolder,FilteringScore,MSA,FilteredMSA):        
     if ProjectMode == 'filter+gap':
         fltaln = filtaln(MSA,GuidanceFolder)
         gfltaln = rmgap(fltaln)
@@ -71,7 +71,6 @@ def cdsMSAfilter_guidance(ProjectMode,GuidanceFolder,FilteringScore,MSA,Filtered
     else:
         print("The mode specified '%s' does not exist" % ProjectMode)
         raise SystemExit(1)
-
     AlignIO.write(gfltaln,FilteredMSA,'fasta')  
 
 if __name__ == "__main__":    
